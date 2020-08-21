d4526eed05adf23f203eadcd462d2e12ba12a171
gregorvwk <69797933+gregorvwk@users.noreply.github.com>
2020-08-21 21:44:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 20
+const isFemale = true
+const driverStatus = true
+
+if (age >= 18) {
+    console.log("Kom maar binnen")
+}else {
+    console.log("Ga weg!")
+}
+
+if (isFemale) {
+    console.log("Kom maar binnen")
+}else {
+    console.log("Ga weg!")
+}
+
+if (driverStatus) {
+    console.log("bob")
+}else {
+    console.log("geen bob")
+}
